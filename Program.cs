fd0a47d35480b1c4866d450cbbcf8851f906ad23
Manny <32347232+Mlpichardo1@users.noreply.github.com>
2018-08-08 00:32:35 -0400

    Running selenium nunit test

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumFirst
 {
     class Program
     {
-        static void Main(string[] args)
+        // First create reference for our browser
+        IWebDriver driver = new ChromeDriver(@"C:\Users\User\");
+        // static void Main(string[] args)
+        // {
+        // }
+
+        [SetUp]
+        public void Initialize()
         {
-            Console.WriteLine("Hello World!");
+            // Navigate to Google Page
+            driver.Navigate().GoToUrl("http://www.google.com");
+            System.Console.WriteLine("Opened URL");
         }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            // Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            // Perform Ops
+            element.SendKeys("executeautomation");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            // Close Driver
+            driver.Close();
+        }
+
     }
 }
